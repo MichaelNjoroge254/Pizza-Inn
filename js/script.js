d98d7c27d6e7fca262e41fec8fb478e2c6765cfd
MichaelNjoroge254 <michael.njoroge@student.moringaschool.com>
2021-06-27 22:15:18 +0300

    initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function Order(flavour, size, crust, toppings, number){
+    this.pizzaFlavour = flavour;
+    this.pizzaSize = size;
+    this.pizzaCrust = crust;
+    this.pizzaToppings = toppings;
+    this.numberOfPizzas = number;
+    //this.PizzaDelivery = deliver
+}
